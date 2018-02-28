a0346b84a6104f093edff9ac74d8feba2e8454d8
SuganJoe <jsuganya91ece@gmail.com>
2018-02-28 09:34:30 -0800

    Update Program.cs
    Make the variable names more readable.

--- Program.cs
+++ Program.cs
@@ -9,41 +9,40 @@ namespace AnimeRecommenderWS
     class Program
     {
         //To find dotproduct of user array and movie array
-        public static double[,] DotProduct(double[,] V1, double[,] V2)
+        public static double[,] DotProduct(double[,] User, double[,] Data)
         {
-            int height = V2.GetUpperBound(0);
-            int Length = V2.GetUpperBound(1);
+            int Data_row = Data.GetUpperBound(0);
+            int Data_column = Data.GetUpperBound(1);
 
 
-            //Transforming the movie array
-            double[,] transformarray = new double[Length + 1, height + 1];
+            //Transforming the movie or data array
+            double[,] transformarray = new double[Data_column + 1, Data_row + 1];
 
-            for (int i = 0; i <= height; i++)
+            for (int i = 0; i <= Data_row; i++)
             {
-                for (int j = 0; j <= Length; j++)
+                for (int j = 0; j <= Data_column; j++)
                 {
-
-                    transformarray[j, i] = V2[i, j];
+                    transformarray[j, i] = Data[i, j];
 
                 }
             }
 
-            int V1r = V1.GetUpperBound(0) + 1;
-            int l2 = transformarray.GetUpperBound(1) + 1;
-            int l3 = V1.GetUpperBound(1) + 1;
-
-            double[,] DParray = new double[V1r, l2];
-            int l1 = DParray.GetUpperBound(0) + 1;
+            int user_row = User.GetUpperBound(0) + 1;
+            int tf_column = transformarray.GetUpperBound(1) + 1;
+      
+            //Initializing DParray - Dotproduct of user array and Transform array
+            double[,] DParray = new double[user_row, tf_column];
+            int DP_row = DParray.GetUpperBound(0) + 1;
 
             //Finding dot product
-            for (int k = 0; k < l1; k++)
+            for (int k = 0; k < DP_row; k++)
             {
-                for (int j = 0; j < l2; j++)
+                for (int j = 0; j < tf_column; j++)
                 {
                     double sum = 0;
-                    for (int i = 0; i < l3; i++)
+                    for (int i = 0; i < (Data_column+1); i++)
                     {
-                        sum = sum + V1[k, i] * transformarray[i, j];
+                        sum = sum + User[k, i] * transformarray[i, j];
 
                     }
 
@@ -62,9 +61,9 @@ namespace AnimeRecommenderWS
             return DParray;
         }
 
-            public static Tuple<double,int> [] ArraySorting(double [,] DParray)
-            {
-
+        //Storing the dotproduct output and the index in a Tuple before sorting
+        public static Tuple<double, int>[] ArraySorting(double[,] DParray)
+        {
 
             //Tuple Creation
             Tuple<double, int>[] Tarray = new Tuple<double, int>[DParray.Length];
@@ -79,7 +78,7 @@ namespace AnimeRecommenderWS
             }
 
 
-            //Sorting of the tuple array
+            //Sorting the tuple array
             for (int i = (Tarray.Length - 1); i > 0; i--)
             {
                 int max_index = i;
@@ -98,12 +97,13 @@ namespace AnimeRecommenderWS
             }
 
             return Tarray;
-            }
+        }
 
+        //To find average of the user profile- converting number of user matrix rows to a single row 
         public static double[,] AveragingUserMatrix(double[,] UserMatrix)
         {
-           // int [,] arr = new int[2,5] { { 1, 0, 1, 2, 3 }, { 1, 2, 3, 1, 1 } };
-            double[,] output = new double [1,UserMatrix.GetUpperBound(1)+1];
+            // int [,] arr = new int[2,5] { { 1, 0, 1, 2, 3 }, { 1, 2, 3, 1, 1 } };
+            double[,] output = new double[1, UserMatrix.GetUpperBound(1) + 1];
 
 
             for (int k = 0; k <= output.GetUpperBound(0); k++)
@@ -113,7 +113,7 @@ namespace AnimeRecommenderWS
                     double sum = 0;
                     for (int i = 0; i <= UserMatrix.GetUpperBound(0); i++)
                     {
-                        sum = sum + UserMatrix[i,j];
+                        sum = sum + UserMatrix[i, j];
                     }
 
                     double Average = sum / (UserMatrix.GetUpperBound(0) + 1);
@@ -121,38 +121,30 @@ namespace AnimeRecommenderWS
                 }
 
             }
-            
-            //for (int i = 0; i <= (output.GetUpperBound(0)); i++)
-            //{
-            //    for (int j = 0; j <= (output.GetUpperBound(1)); j++)
-            //    {
-            //        Console.WriteLine(output[i, j]);
-            //    }
-            //}
-             return output;
+            return output;
         }
-        
+
 
 
         static void Main(string[] args)
-            {
-                Utility handle = new Utility();
+        {
+            Utility handle = new Utility();
 
-                //Path of anime movies dataset
-                string moviesfilepath = @"D:\Courses\Winter2018\WebServices\Project\Updated1\anime-recommender-db-master\out\Processed\database.csv";
-                string[,] BigMatrix = handle.LoadCsv(moviesfilepath);
+            //Path of anime movies dataset
+            string moviesfilepath = @"D:\Courses\Winter2018\WebServices\Project\Updated1\anime-recommender-db-master\out\Processed\database.csv";
+            string[,] DataMatrix = handle.LoadCsv(moviesfilepath);
 
-                //Path of user profile data
-                string userdatapath = @"D:\Courses\Winter2018\WebServices\Project\Updated1\anime-recommender-db-master\out\Processed\UserProfile.csv";
-                string[,] UserMatrix = handle.LoadCsv(userdatapath);
+            //Path of user profile data
+            string userdatapath = @"D:\Courses\Winter2018\WebServices\Project\Updated1\anime-recommender-db-master\out\Processed\UserProfile.csv";
+            string[,] UserMatrix = handle.LoadCsv(userdatapath);
 
-            double[,] BigMatrix1 = handle.SliceArray(BigMatrix);
+            double[,] DataMatrix1 = handle.SliceArray(DataMatrix);
             double[,] UserMatrix1 = handle.SliceArray(UserMatrix);
-            
+
             double[,] UserMatrix2 = AveragingUserMatrix(UserMatrix1);
 
             //To find similarity between user profile and movie database
-            double[,] DParray = DotProduct(UserMatrix2, BigMatrix1);
+            double[,] DParray = DotProduct(UserMatrix2, DataMatrix1);
             Tuple<double, int>[] Sortedarray = ArraySorting(DParray);
 
             int greatestfives = 5;
@@ -166,9 +158,9 @@ namespace AnimeRecommenderWS
 
             for (int i = 0; i < greatestfives; i++)
             {
-                Console.WriteLine(BigMatrix[(order[i] + 1), 0]);
+                Console.WriteLine(DataMatrix[(order[i] + 1), 0]);
             }
         }
-        }
     }
+}
 
